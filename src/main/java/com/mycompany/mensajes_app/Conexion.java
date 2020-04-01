6d99006395f79c7b2e2b451bc8536667fe80c31f
Ulzahk <ulzahk@gmail.com>
2020-04-01 16:45:50 -0500

    Diseño de estructura base para las clases mensajes

--- src/main/java/com/mycompany/mensajes_app/Conexion.java
+++ src/main/java/com/mycompany/mensajes_app/Conexion.java
@@ -12,9 +12,6 @@ public class Conexion {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","");
-            if(connection != null){
-                System.out.println("Conexión Exitosa");
-            }
         } catch (SQLException e) {
             System.out.println(e);
         }
--- src/main/java/com/mycompany/mensajes_app/Inicio.java
+++ src/main/java/com/mycompany/mensajes_app/Inicio.java
@@ -1,17 +1,44 @@
 package com.mycompany.mensajes_app;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 public class Inicio {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         
-        Conexion conexion = new Conexion();
+        int opcion = 0;
         
-        try(Connection cnx = conexion.get_connection()) {
+        do {            
+            System.out.println("--------------------------");
+            System.out.println(" Aplicación de Mensajes");
+            System.out.println(" 1. Crear un Mensaje");
+            System.out.println(" 2. Listar Mensajes");
+            System.out.println(" 3. Editar Mensaje");
+            System.out.println(" 4. Eliminar Mensaje");
+            System.out.println(" 5. Salir"); 
+            //Leemos la opción del usuario
+            opcion = sc.nextInt();
             
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+            switch(opcion){
+                case 1: 
+                    MensajesService.crearMensaje();
+                    break;
+                case 2:
+                    MensajesService.listarMensajes();
+                    break;
+                case 3:
+                    MensajesService.borrarMensaje();
+                    break;
+                case 4:
+                    MensajesService.editarMensaje();
+                    break;
+                default:
+                    System.out.println("Por favor seleccione una opción valida");
+                    break;
+            }
+        } while (opcion != 5);
+        Conexion conexion = new Conexion();
         
     }
 }
