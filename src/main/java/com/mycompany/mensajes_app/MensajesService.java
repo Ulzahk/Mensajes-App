ee5178764e786775dab8cd4ab1fecb5acbbaa2b8
Ulzahk <ulzahk@gmail.com>
2020-04-02 08:36:01 -0500

    Desarrollo de los metodos actualizar datos

--- src/main/java/com/mycompany/mensajes_app/MensajesService.java
+++ src/main/java/com/mycompany/mensajes_app/MensajesService.java
@@ -33,6 +33,15 @@ public class MensajesService {
     }
     
     public static void editarMensaje(){
+        System.out.println("Escribe tu nuevo mensaje");
+        String mensaje = sc.nextLine();
         
+        System.out.println("Indica el ID del mensaje a editar");
+        int id_mensaje = sc.nextInt();
+        
+        Mensajes actualizacion = new Mensajes();
+        actualizacion.setMensaje(mensaje);
+        actualizacion.setId_mensaje(id_mensaje);
+        MensajesDAO.actualizarMensajeBD(actualizacion);
     }
 }
